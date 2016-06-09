a8efc6e039f043657e8c2e882f62bfa2e2f89c9e
Francesco Giordano <francesco.giordano@buildo.io>
2016-06-10 01:16:32 +0200

    update separator

--- github-bitbar-counter.js
+++ github-bitbar-counter.js
@@ -42,7 +42,7 @@ export default (config) => (...githubQueries) => {
   )
 
   const showHeader = (...results) => ({
-    text: results.map(r => r.total_count).join('/'),
+    text: results.map(r => r.total_count).join(' | '),
     emojize: true,
     color: BLUE,
     image: GITHUB_ICON,
